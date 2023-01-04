46a3e0c15ee09b0a3792fd25ee4ad33252b3011f
Dinara <dinaranav1987@gmail.com>
2023-01-04 14:43:14 +0400

    feat: add fetching and displaying products on main page

--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -1,15 +1,77 @@
-import React from 'react';
-import TestForMain from '../../Components/TestForMain';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+// import TestForMain from '../../Components/TestForMain';
 
 function Main() {
-  const testArr = [1, 2, 3, 4];
+  // const { cartItems } = props;
+  // const testArr = [1, 2, 3, 4];
+  const [products, setProducts] = useState([]);
 
-  // Рендерим 4 компонента на основе массива, передаем в каждый компонент пропсы из массива
-  // При таком рендере важно указывать key для каждого компонента. Он должен быть уникальным
+  const fetchProducts = async () => {
+    const response = await fetch('https://dummyjson.com/products?limit=10');
+    const productsList = await response.json();
+    setProducts(productsList.products);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  type TProductsItem = {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    category: string;
+    brand: string;
+    discountPercentage: number;
+    images: string[];
+    rating: number;
+    stock: number;
+    thumbnail: string;
+  }
+
   return (
     <>
       {
-            testArr.map((item) => <TestForMain key={item} number={item} />)
+            products.map((item: TProductsItem) => (
+              <div key={item.id}>
+                <h3>{item.title}</h3>
+                <img src={item.thumbnail} alt={item.title} />
+                <dl>
+                  <dt>Category: </dt>
+                  <dd>{item.category}</dd>
+                </dl>
+                <dl>
+                  <dt>Brand: </dt>
+                  <dd>{item.brand}</dd>
+                </dl>
+                <dl>
+                  <dt>Price: </dt>
+                  <dd>
+                    $
+                    {item.price}
+                  </dd>
+                </dl>
+                <dl>
+                  <dt>Discount: </dt>
+                  <dd>
+                    {item.discountPercentage}
+                    %
+                  </dd>
+                </dl>
+                <dl>
+                  <dt>Rating: </dt>
+                  <dd>{item.rating}</dd>
+                </dl>
+                <dl>
+                  <dt>Stock: </dt>
+                  <dd>{item.stock}</dd>
+                </dl>
+                <button type="button">Add to cart</button>
+                <Link to={`/${item.id}`}>Details</Link>
+              </div>
+            ))
         }
     </>
   );
