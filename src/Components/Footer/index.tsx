f814686fe5764763dac4e1d74be9d1bb267171fe
Dinara <dinaranav1987@gmail.com>
2023-01-10 20:23:51 +0400

    fix: upgrade footer

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -6,13 +6,13 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <p>
+        <p className={styles.linksContainer}>
           {'Developed by '}
           <a className={styles.footer__link} href="https://github.com/Buffik">Buffik</a>
           {', '}
           <a className={styles.footer__link} href="https://github.com/dinara-n">dinara-n</a>
         </p>
-        <p>© 2023</p>
+        <p>Online Store © 2023</p>
         <a href="https://rs.school/js/">
           <img src={logo} width="55.28" height="20.53" alt="" />
         </a>
