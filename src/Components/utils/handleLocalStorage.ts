a4995021456e5486165ceee4a9fc05f9b47a5233
Buffik <62429354+Buffik@users.noreply.github.com>
2023-01-31 20:49:04 +0300

    Fix utils types (#31)
    * fix: response types at API services
    
    * feat: fix types at util function handle localstorage

--- src/Components/utils/handleLocalStorage.ts
+++ src/Components/utils/handleLocalStorage.ts
@@ -1,4 +1,6 @@
-function handleLocalStorage() {
+import { TProductPartialProps } from '../../types/types';
+
+function handleLocalStorage(): TProductPartialProps[] | [] {
   const currentProducts = window.localStorage.getItem('productsByTeamDiBu');
   if (currentProducts) return JSON.parse(currentProducts);
   return [];
